909a662bcdad3b3ffaff105cc3aba491e00e3ac0
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-11-06 19:43:53 +0700

    Update fetch data for dashboar

--- client/src/pages/DashBoard/index.jsx
+++ client/src/pages/DashBoard/index.jsx
@@ -1,22 +1,65 @@
-import React from "react";
-import { Box, Divider, HStack, Spacer, Text, VStack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Divider,
+  HStack,
+  Spacer,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Navbar from "../../components/Navbar";
 import Profile from "./component/Profile";
 import Footer from "../../components/Footer";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 export default function DashBoard(props) {
+  const [user, setUser] = useState([]);
+
+  const loggedInUser = useSelector(
+    (state) => state.login.user
+  );
+
+  const selectedUser = user.find(
+    (user) => user.id === +loggedInUser.id
+  );
+  console.log("selecteduser", selectedUser);
+
+  const fetchUser = async (req, res) => {
+    try {
+      const res = await axios.get(
+        "http://localhost:8000/user"
+      );
+      setUser(res?.data?.data);
+      return res?.data?.data;
+    } catch (err) {
+      throw err;
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <Box>
       <Navbar display={"none"} />
-      <Box p={"0 3.5em"} mt={"10em"} mb={"2em"} minHeight={"100vh"}>
+      <Box
+        p={"0 3.5em"}
+        mt={"10em"}
+        mb={"2em"}
+        minHeight={"100vh"}
+      >
         <Box display={"flex"}>
           <Box w={"30%"}>
             <VStack align={"stretch"}>
               <Box p={".25em .5em"} borderRadius={".5em"}>
                 <Text as={"b"}>DASHBOARD</Text>
               </Box>
-              <Divider borderColor={"#192655"} borderWidth={"2px"} />
+              <Divider
+                borderColor={"#192655"}
+                borderWidth={"2px"}
+              />
               <Box p={".25em .5em"}>
                 <Link to={"/my-event"}>
                   <Text>Event Saya</Text>
@@ -25,7 +68,10 @@ export default function DashBoard(props) {
               <Box p={".25em .5em"} borderRadius={".5em"}>
                 <Text as={"b"}>PROFILE</Text>
               </Box>
-              <Divider borderColor={"#192655"} borderWidth={"2px"} />
+              <Divider
+                borderColor={"#192655"}
+                borderWidth={"2px"}
+              />
               <Box p={".25em .5em"}>
                 {props.input}
                 <Text>Profile</Text>
@@ -41,7 +87,6 @@ export default function DashBoard(props) {
           </Box>
           <Spacer m={"1em"} />
           <Box w={"full"}>
-            {/* <EventSaya /> */}
             <Profile display={props.display} />
           </Box>
         </Box>
