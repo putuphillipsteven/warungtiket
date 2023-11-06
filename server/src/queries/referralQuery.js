364955fc6cb78fe5be4fc80138a14a69d296f8df
sam <wllmsawi@gmail.com>
2023-11-06 14:16:01 +0700

    Update referralQuery.js

--- server/src/queries/referralQuery.js
+++ server/src/queries/referralQuery.js
@@ -2,18 +2,26 @@ const db = require("../models");
 const { Op } = require("sequelize");
 const referral = db.referral;
 
-const createReferralQuery = async (
-  referralCode,
-  isUse,
-  eventId,
-  userId
-) => {
+const createReferralQuery = async (referralCode, isUse, eventId, userId) => {
   try {
     const res = await referral.create({
       referralCode,
       isUse,
       eventId,
       userId,
+    });
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const findRefferalQuery = async ({ referralCode = null }) => {
+  try {
+    const res = await referral.findOne({
+      where: {
+        referralCode,
+      },
     });
     return res;
   } catch (err) {
